f943dfdd696c3769d3e2d222b92a640d75a9d813
Sebastian Marroquin <sebasmarro10@gmail.com>
2021-02-03 22:31:42 -0600

    Problems in the predict model | Fix ASAP

---
+++ fakeNewsModels/example-news/README.md
@@ -0,0 +1,3 @@
+# News Examples
+
+Inside this folder, we will store news that are real and false in a file, this in order to test the performance of our classifier.
--- site/applicationSite/app.py
+++ site/applicationSite/app.py
@@ -1,5 +1,6 @@
 # Import the neccessary libraries
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import pickle
 
 # Set the flask app
 app = Flask(
@@ -9,16 +10,35 @@ app = Flask(
     template_folder='templates'
 )
 
-# Home page
-@app.route('/', methods = ['GET'])
+# Load the model
+model = pickle.load(open('passiveAgressiveModel.pkl', 'rb'))
+# Load the Tf-idf vectorizer
+tfidf = pickle.load(open('tfidf.pkl', 'rb'))
+
+# # Home page
+@app.route('/')
 def index():
     return render_template('index.html')
 
 # App page
-@app.route('/tantei', methods = ['GET', 'POST'])
+@app.route('/tantei')
 def tantei():
     return render_template('tantei.html')
 
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+# Predicted result
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    # Fix this part
+    predict = model.predict(tfidf.transform[message])
+
+    return render_template('result.html', prediction = predict)
+
 # Running in port 3200
 if __name__ == '__main__':
+    # Run the app
     app.run(port = 3200, debug = True)
